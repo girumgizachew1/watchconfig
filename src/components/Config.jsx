b0363fe520904a0e5acd3c2994959f7a3da23a36
girum <girumegizachew@gmail.com>
2023-03-16 12:44:24 +0300

    added colors to band

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useCustomization } from '../context/custumization'
+import { bandcolors, useCustomization } from '../context/custumization'
 
 function Config() {
-    const { material, setMaterial } = useCustomization();
+    const { material, setMaterial, bandcolor , setBandcolor } = useCustomization();
     console.log(material);
   return (
     <div className='w-[85vh] h-[85vh] bg-gray-200 rounded-2xl shadow-zinc-200 '>
@@ -11,6 +11,23 @@ function Config() {
             <div className='flex flex-row text-xs space-x-2' >
                 <button className='bg-gray-300 px-2 py-1 rounded-3xl text-white ' onClick={() => setMaterial('sololoop')} >Solo Loop</button>
                 <button className='bg-gray-300 px-2 py-1 rounded-3xl text-white ' onClick={() => setMaterial('leather')} >Leather</button>
+            </div>
+            <div className='flex flex-col' >
+                <h1 className='text-sm' >color</h1>
+                <div className='flex flex-row space-x-4 '>
+                    {bandcolors.map((item, index) => (
+                        <div
+                        key={index}
+                        className=""
+                        onClick={() => setBandcolor(item)}
+                        >   
+                        <div className='w-7 border-gray-300 shadow-lg border h-7 rounded-full' style={{ backgroundColor: item.color }} />
+                        <div className='text-gray-700 text-xs'>
+                            {item.name}
+                        </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     </div>
--- src/context/custumization.jsx
+++ src/context/custumization.jsx
@@ -2,14 +2,41 @@ import { createContext, useContext, useState } from "react";
 
 const CustomizationContext = createContext({});
 
+export const bandcolors = [
+     {
+        name: "white",
+        color:  "#ffffff",
+    },
+    {
+        name: "black",
+        color:  "#000000",
+    },
+   
+    {
+        name: "Lime",
+        color:  "#00ff00",
+    },
+    {
+        name: "yellow",
+        color:  "#ffff00",
+    },
+    {
+        name: "cyan",
+        color:  "#00ffff",
+    }
+]
+
+
 export const CustomizationProvider = ( props ) => {
     const [material , setMaterial] = useState("leather")
-    
+    const [bandcolor, setBandcolor] =useState(bandcolors[0])
     return(
         <CustomizationContext.Provider 
         value={{ 
             material, 
             setMaterial,
+            bandcolor,
+            setBandcolor,
             }}
         >
             { props.children }
